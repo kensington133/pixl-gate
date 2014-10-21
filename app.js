784510f17f4d504e77e88642c324f7079d75dab9
Ben Short <benshort.mouse@gmail.com>
2014-10-21 18:31:06 +0100

    changing some files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var register = require('./routes/register/index');
+var register = require('./routes/register');
 
 var app = express();
 
